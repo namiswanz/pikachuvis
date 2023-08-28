7abb9f5676816eda3495aa0fe6fb04f780bf4068
monkeydluffy76 <124869601+monkeydluffy76@users.noreply.github.com>
2023-08-28 11:19:57 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = "21609776"
 api_hash = "61f2558d634df353a2a9806661f4b091"
-bot_token = "6317224996:AAG-fIs-iPGfaVmHmePIPspVYMgVpSTBAYA"
-sudo_groups = -1001849087530
+bot_token = "6610143533:AAFvYZMQtcJeFenFlOAVTRP1pMo8F_f47HI"
+sudo_groups = -1001912090653
